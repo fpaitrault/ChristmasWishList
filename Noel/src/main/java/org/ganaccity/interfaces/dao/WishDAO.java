78c911a8bd03e7c89df88d1c3e711b91850d40da
fpaitrault <florentpaitrault@yahoo.fr>
2014-08-31 18:07:28 +0200

    Added periodic task for automatic email sending

--- Noel/src/main/java/org/ganaccity/interfaces/dao/WishDAO.java
+++ Noel/src/main/java/org/ganaccity/interfaces/dao/WishDAO.java
@@ -1,7 +1,9 @@
 package org.ganaccity.interfaces.dao;
 
+import java.util.List;
+
 import org.ganaccity.mdl.Wish;
 
 public interface WishDAO extends GenericDAO<Wish> {
-
+    List<Wish> getHotWishes();
 }
