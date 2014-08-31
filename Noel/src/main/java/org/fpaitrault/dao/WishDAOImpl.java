9cadb37b0a88bc1bf6d797fa3da8b51fde816671
fpaitrault <florentpaitrault@yahoo.fr>
2014-08-31 18:07:28 +0200

    Added periodic task for automatic email sending

--- Noel/src/main/java/org/fpaitrault/dao/WishDAOImpl.java
+++ Noel/src/main/java/org/fpaitrault/dao/WishDAOImpl.java
@@ -1,5 +1,7 @@
 package org.fpaitrault.dao;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,11 +28,36 @@ public class WishDAOImpl extends GenericDAOImpl<Wish> implements WishDAO {
     public List<Wish> readAll() {
         List<Wish> res = null;
         List<Wish> toBeRemoved = new LinkedList<Wish>();
+        
         synchronized (getSession()) {
             Query query = getSession().createQuery("from " + Wish.class.getName());
             res = query.list();
         }
+
+        return cleanupWishes(res, toBeRemoved);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Wish> getHotWishes() {
+        List<Wish> res = null;
+        List<Wish> toBeRemoved = new LinkedList<Wish>();
         
+        synchronized (getSession()) {
+            Query query = getSession().createQuery("from " + Wish.class.getName() +
+                    " where updated > :date").setDate("date", subtractDay(new Date()));
+            res = query.list();
+        }
+        
+        return cleanupWishes(res, toBeRemoved);
+    }
+
+    private Date subtractDay(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        return cal.getTime();
+    }
+    private List<Wish> cleanupWishes(List<Wish> res, List<Wish> toBeRemoved) {
         //Remove wishes that shall not be visible !!!!!!!
         User user = authService.getUserCredential();
         for(Wish wish : res) {
@@ -41,8 +68,7 @@ public class WishDAOImpl extends GenericDAOImpl<Wish> implements WishDAO {
             }
         }
         res.removeAll(toBeRemoved);
+        
         return res;
     }
-
-
 }
--- Noel/src/main/java/org/fpaitrault/interfaces/dao/WishDAO.java
+++ Noel/src/main/java/org/fpaitrault/interfaces/dao/WishDAO.java
@@ -1,7 +1,9 @@
 package org.fpaitrault.interfaces.dao;
 
+import java.util.List;
+
 import org.fpaitrault.mdl.Wish;
 
 public interface WishDAO extends GenericDAO<Wish> {
-
+    List<Wish> getHotWishes();
 }
