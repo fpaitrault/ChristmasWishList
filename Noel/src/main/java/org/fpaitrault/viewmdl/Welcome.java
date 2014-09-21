a76813c799b70dd00c0c374c3476817ece4f249c
fpaitrault <florentpaitrault@yahoo.fr>
2014-09-21 18:40:09 +0200

    Added countdown in welcome page Improved email template

---
+++ Noel/src/main/java/org/fpaitrault/viewmdl/Welcome.java
@@ -0,0 +1,67 @@
+package org.fpaitrault.viewmdl;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.fpaitrault.interfaces.AuthenticationService;
+import org.fpaitrault.interfaces.dao.UserDAO;
+import org.fpaitrault.mailing.EMailFactory;
+import org.fpaitrault.mdl.DeviceMode;
+import org.fpaitrault.mdl.User;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.ClientInfoEvent;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
+public class Welcome {
+
+    public Welcome() {
+    }
+    
+    
+    public String getMessage() throws IOException, TemplateException {
+        Map<String,Object> model = new HashMap<String, Object>();
+        //Create data model
+        //Calculate countdown
+        Date today = Calendar.getInstance().getTime();
+        Calendar cal = GregorianCalendar.getInstance();
+        cal.set(today.getYear()+1900, Calendar.DECEMBER, 25);
+        Date christmas = cal.getTime();
+        long diff = christmas.getTime() - today.getTime();
+        long numDays = diff/(1000*60*60*24);
+        model.put("countdown_d",  Long.toString(numDays/10) );
+        model.put("countdown_u",  Long.toString(numDays%10) );
+        
+        //configure page processor
+        Configuration cfg = new Configuration();
+        cfg.setClassForTemplateLoading(EMailFactory.class,"/");
+        cfg.setObjectWrapper(new DefaultObjectWrapper());
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        
+        Template tmpl = cfg.getTemplate("welcome.tpl");
+        
+        StringWriter writer = new StringWriter();
+        
+        tmpl.process(model, writer);
+        
+        return writer.toString();
+    }
+    
+}
